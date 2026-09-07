4e30360b4d418059b994d3939a3a0676efbb5dea
wdhysrz <2918159660@qq.com>
2026-09-07 23:53:57 +0800

    更新 x30e-pro-ubi.mk

--- vanilla/x30e-pro/x30e-pro-ubi.mk
+++ vanilla/x30e-pro/x30e-pro-ubi.mk
@@ -1,21 +1,23 @@
-
 define Device/ruijie_rg-x30e-pro-ubi
   DEVICE_VENDOR := Ruijie
   DEVICE_MODEL := RG-X30E Pro (UBI)
   DEVICE_DTS := mt7981b-ruijie-rg-x30e-pro-ubi
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES :=kmod-mt7915e kmod-mt7981-firmware \
-  mt7981-wo-firmware kmod-mtd-rw
+	mt7981-wo-firmware kmod-mtd-rw
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_IN_UBI := 1
   UBOOTENV_IN_UBI := 1
   KERNEL := kernel-bin | gzip
   KERNEL_INITRAMFS := kernel-bin | lzma | \
-  fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
-  IMAGES := sysupgrade.itb
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd
+  IMAGES := sysupgrade.itb factory.ubi
   IMAGE/sysupgrade.itb := append-kernel | \
-  fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb \
-  external-with-rootfs | pad-rootfs | append-metadata
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb \
+	external-with-rootfs | pad-rootfs | append-metadata
+  IMAGE/factory.ubi := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb \
+	external-with-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += ruijie_rg-x30e-pro-ubi
